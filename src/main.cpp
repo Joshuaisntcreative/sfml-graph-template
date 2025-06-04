7bc4acda86ae69aa315c4c932ce39b32ef659f3e
Joshuaisntcreative <joshuajainarine@gmail.com>
2025-06-04 10:58:16 -0400

    sine wave

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <cmath>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({800u,600u}), "CMake SFML Project");
     window.setFramerateLimit(144);
-
+//Each vertex holds a position and a color
+std::vector<sf::Vertex> graphPoints;
+    for (float x = 0; x < 800; x += 1) {
+        float y = 300 + std::sin(x * 0.01f) * 100;  // sine wave centered at y = 300
+        graphPoints.push_back({sf::Vector2f(x, y), sf::Color::Green});
+    }
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -15,7 +22,8 @@ int main()
             }
         }
 
-        window.clear();
+        window.clear(sf::Color::Black);
+        window.draw(&graphPoints[0], graphPoints.size(), sf::PrimitiveType::LineStrip);   
         window.display();
     }
 }
